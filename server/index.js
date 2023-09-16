29ee7cf6c23462a71f093fddf330143c937dc797
eliasgebreamanuel <eliasgamanuel@gmail.com>
2023-09-16 23:27:27 +0300

    Third Commit

--- server/index.js
+++ server/index.js
@@ -11,8 +11,13 @@ import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import {register} from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts} from "./data/index.js";
 
 /* CONFIGURATION */
 
@@ -44,12 +49,12 @@ const upload = multer({ storage });
 
 
 /* ROUTES WITH FILES */ 
-app.post("/auth/register", upload.single("picture"),verifyToken, register);
-
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
-
+app.use("/posts", postRoutes);
 /*  MONGOOSE SETUP */
 
 const PORT = process.env.PORT || 6001;
@@ -59,5 +64,10 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+
 })
 .catch((error) => console.log(`${error} did not connect`));
